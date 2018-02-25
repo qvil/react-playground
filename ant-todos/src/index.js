4e71dad5a633cdeab8cb19ad1d785a7515a96b4f
Taesu Hyeon <qvil1127@gmail.com>
2018-02-25 21:56:27 +0900

    [Firebase] Complete addTodo test code

--- ant-todos/src/index.js
+++ ant-todos/src/index.js
@@ -4,7 +4,30 @@ import { AppContainer } from 'containers';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+// Firebase
+import * as firebase from 'firebase';
+// Custom
 import reducer from 'ducks';
+
+var config = {
+  apiKey: "AIzaSyBaEKUTLA4sd2VakMU3lLQZlfZ1yLH5CPI",
+  authDomain: "ant-todos.firebaseapp.com",
+  databaseURL: "https://ant-todos.firebaseio.com",
+  projectId: "ant-todos",
+  storageBucket: "",
+  messagingSenderId: "223321773229"
+};
+firebase.initializeApp(config);
+
+// function addTodoToDB(id, text, completed) {
+//   firebase.database().ref('todos').set({
+//     id,
+//     text,
+//     completed,
+//   });
+// }
+
+// addTodoToDB(0, "Play", false);
 
 const store = createStore(reducer);
 
