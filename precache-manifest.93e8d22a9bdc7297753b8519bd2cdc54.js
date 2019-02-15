8fb4df2420a7591d6d9dd6468301e5dcace8fc30
Taesu Hyeon <qvil1127@gmail.com>
2019-02-16 02:01:14 +0900

    Updates

---
+++ precache-manifest.93e8d22a9bdc7297753b8519bd2cdc54.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe3d9d3d85380b7e0029",
+    "url": "/react-playgound/lab/static/js/runtime~main.fe3d9d3d.js"
+  },
+  {
+    "revision": "cb98fd3329de6045e86f",
+    "url": "/react-playgound/lab/static/js/main.cb98fd33.chunk.js"
+  },
+  {
+    "revision": "97c2f15b8790351eac17",
+    "url": "/react-playgound/lab/static/js/1.97c2f15b.chunk.js"
+  },
+  {
+    "revision": "4437313b3276518bad47c4f6cb5daf6e",
+    "url": "/react-playgound/lab/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-playgound/precache-manifest.63ef08a496ce3f30e22d2f9b4c4455c7.js"
+  "/react-playgound/lab/precache-manifest.93e8d22a9bdc7297753b8519bd2cdc54.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-playgound/index.html", {
+workbox.routing.registerNavigationRoute("/react-playgound/lab/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
