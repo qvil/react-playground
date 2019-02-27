df7c8b1332eb19605949ec17c35c18e3b32c778c
Taesu Hyeon <qvil1127@gmail.com>
2019-02-28 02:44:05 +0900

    Separate Todo Memo.Component for performance

--- lab/src/components/hooks/useReducer/TodoList/TodoList.jsx
+++ lab/src/components/hooks/useReducer/TodoList/TodoList.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useReducer } from "react";
 import { ADD_TODO } from "./constants";
 import reducer from "./reducer";
+import { TodoContext } from "./store";
 
 const initialState = [{ id: 0, text: "Lean React Hooks!" }];
+
+const Todo = React.memo(({ todo }) => {
+  console.log("Todo render!");
+  return <li>{todo.text}</li>;
+});
 
 const TodoList = () => {
   const [todo, setTodo] = useState("");
@@ -15,22 +21,24 @@ const TodoList = () => {
   };
 
   return (
-    <div>
-      <h3>TodoList</h3>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Add todo"
-          value={todo}
-          onChange={e => setTodo(e.target.value)}
-        />
-        <button>Add</button>
-      </form>
-      <ol>
-        {state.map(todo => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ol>
-    </div>
+    <TodoContext.Provider value={1}>
+      <div>
+        <h3>TodoList</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Add todo"
+            value={todo}
+            onChange={e => setTodo(e.target.value)}
+          />
+          <button>Add</button>
+        </form>
+        <ol>
+          {state.map(todo => (
+            <Todo key={todo.id} todo={todo} />
+          ))}
+        </ol>
+      </div>
+    </TodoContext.Provider>
   );
 };
 
