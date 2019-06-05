88720594645e8e0eb975ae38f56e770f8baeaf95
Taesu Hyeon <qvil1127@gmail.com>
2019-06-06 03:26:10 +0900

    Add useConfirm(Complete)

--- pages/hooks/use-confirm/index.jsx
+++ pages/hooks/use-confirm/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useConfirm } from "./useConfirm";
 
 const UseConfirmPage = () => {
-  return <div>UseConfirmPage</div>;
+  const deleteWorld = () => console.log("Delete the world");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
+  return (
+    <div>
+      <h1>UseConfirmPage</h1>
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
 };
 
 export default UseConfirmPage;
